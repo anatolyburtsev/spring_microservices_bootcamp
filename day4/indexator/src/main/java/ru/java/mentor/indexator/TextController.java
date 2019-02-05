90a983af6cc58f169b73274ad58d8c457a1468d3
aburtsev <aburtsev@ozon.ru>
2019-02-05 22:37:09 +0300

    add zuul gateway

--- day4/indexator/src/main/java/ru/java/mentor/indexator/TextController.java
+++ day4/indexator/src/main/java/ru/java/mentor/indexator/TextController.java
@@ -2,6 +2,7 @@ package ru.java.mentor.indexator;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class TextController {
     public Mono<Integer> processText(@RequestBody String body) throws URISyntaxException {
         log.info("got body: " + body);
         return textSplitter.process(body);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
     }
 }
---
+++ day4/zuulgateway/src/main/java/ru/java/mentor/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,16 @@
+package ru.java.mentor.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+}
+
