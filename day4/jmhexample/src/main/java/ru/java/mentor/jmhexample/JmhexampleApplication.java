babc63397dc4f20219b1a03f465c65f4ef1a6865
aburtsev <aburtsev@ozon.ru>
2019-02-06 10:42:57 +0300

    add perf test on smart and naive intersections

---
+++ day4/jmhexample/src/main/java/ru/java/mentor/jmhexample/JmhexampleApplication.java
@@ -0,0 +1,14 @@
+package ru.java.mentor.jmhexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmhexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmhexampleApplication.class, args);
+	}
+
+}
+
